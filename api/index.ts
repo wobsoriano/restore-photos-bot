c0cd68746e197441e5ab59272783e26c7de0c6e3
wobsoriano <sorianorobertc@gmail.com>
2023-11-27 11:28:05 -0800

    fix types

--- api/index.ts
+++ api/index.ts
@@ -4,7 +4,8 @@ import { startVercel } from '../src';
 export default async function handle(req: VercelRequest, res: VercelResponse) {
   try {
     await startVercel(req, res);
-  } catch (e: any) {
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+}  catch (e: any) {
     res.statusCode = 500;
     res.setHeader('Content-Type', 'text/html');
     res.end('<h1>Server Error</h1><p>Sorry, there was a problem</p>');
